76b9c113b25875f963b63e33052e632ee274bcd5
Ivan <sim675kn@gmail.com>
2017-04-20 00:54:32 +0300

    CleanUp

--- devvv/views.py
+++ devvv/views.py
@@ -8,14 +8,7 @@ import logging
 
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect, HttpResponse
-#from rest_framework.viewsets import ModelViewSet
-from django.shortcuts import render
-from django.urls import reverse
 from django.views.generic import View, TemplateView
-#from cover_images.models import CoverImage
-#from inventory.models import FeaturedBoat
-#from destinations.models import DestinationAlias, AliasSerializer
-from django.contrib.gis.geoip import GeoIP
 
 
 class IndexView(TemplateView):
