3c207834e62c1164e8fe82ae037176f5619a812e
Ivan <sim675kn@gmail.com>
2017-07-10 23:04:29 +0300

    Added requirements

--- devvv/views.py
+++ devvv/views.py
@@ -1,11 +1,3 @@
-from django.shortcuts import render
-import datetime
-import json
-import requests
-import hashlib
-import urllib
-import logging
-
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect, HttpResponse
 from django.views.generic import View, TemplateView
